176a083293b20fde3265dcba77850053868b9ef0
Selene <149156667+joanapq@users.noreply.github.com>
2024-01-07 16:05:11 +0000

    completed function to get a random element from an array

--- assets/script.js
+++ assets/script.js
@@ -128,7 +128,8 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
 // Function to generate password with user input
